7ce867bc1d6a30c85e86acae8001a9dbc2f67821
FreeGeng <nick830107@gmail.com>
2015-12-23 21:16:54 +0800

    decoder ver1

---
+++ UDP_Can_Ver_Debug/src/cof/server/interfaces/CentralDataCenter.java
@@ -0,0 +1,15 @@
+package cof.server.interfaces;
+import java.util.ArrayList;
+
+public interface CentralDataCenter {
+	public void addPlayer(int clientNo);
+	public void addUnit(String type, int level, int clientno, int aisle);
+	public void addMessenger(int clientNo);
+	public void startGame();
+	public void callCapitalCitySkill (int clientNo, String skillName);
+	public void callTowerSkill (int clientNo);
+	public void callTowerUpgrade (int clientNo, String updateName);
+	public ArrayList<String> getUpdateInfo();
+	public void startUpdatingThread ();
+	public ArrayList<Integer> getEndScore();
+}
